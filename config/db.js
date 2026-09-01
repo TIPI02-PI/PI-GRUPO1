be023c39c90db8abec6cb961d11e32fcc64d8dd7
Grazielly Lacerda <graziilacerda19@gmail.com>
2026-09-01 18:28:35 -0300

    atualizando com a conexão do arquivo .env
    Updated database connection to use environment variables for configuration.

--- config/db.js
+++ config/db.js
@@ -1,10 +1,23 @@
 import mysql from "mysql2/promise.js"
 
+import "dotenv/config"
+
+dotenv.config()
+
 const conexao = await mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: "siteLivros"
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 })
 
 export default conexao
+
+// const conexao = await mysql.createConnection({
+//     host: "localhost",
+//     user: "root",
+//     password: "",
+//     database: "siteLivros"
+// })
+
